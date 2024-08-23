9b7bf5243ba3c16d9ad13a51d86866a1c183fb04
Martin Jensen <m.rd.jensen42@gmail.com>
2024-08-23 12:27:10 +0200

    comments out how main should look

--- base_main.tf
+++ base_main.tf
@@ -1,3 +1,4 @@
+/*
 terraform {
   required_providers {
     intersight = {
@@ -11,4 +12,4 @@ provider "intersight" {
   apikey    = ""
   secretkey = ""
   endpoint  = "https://eu-central-1.intersight.com"
-}
+}*/
